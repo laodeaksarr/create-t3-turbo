431e693eede07df2aed743a50550e169788ac04d
Aksar La'ode <101698503+laodeaksarr@users.noreply.github.com>
2022-11-21 02:40:51 +0000

    edit

--- apps/nextjs/src/pages/api/auth/[...nextauth].ts
+++ apps/nextjs/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 
-import { authOptions } from '@acme/auth';
+import { authOptions } from "@acme/auth";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export default NextAuth(authOptions);
