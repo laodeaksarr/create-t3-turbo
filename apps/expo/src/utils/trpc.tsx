f71e91f82b904f5c3d9a584bd98ad03c11ff2dc2
Aksar La'ode <101698503+laodeaksarr@users.noreply.github.com>
2022-12-17 04:58:28 +0000

    refresh token

--- apps/expo/src/utils/trpc.tsx
+++ apps/expo/src/utils/trpc.tsx
@@ -1,10 +1,27 @@
 import { createTRPCReact } from "@trpc/react-query";
+import { AppRouter } from "@aksar/api";
 
 /**
  * Extend this function when going to production by
  * setting the baseUrl to your production API URL.
  */
 import Constants from "expo-constants";
+
+/**
+ * A wrapper for your app that provides the TRPC context.
+ * Use only in _app.tsx
+ */
+import React /*, { useEffect }*/ from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { transformer } from "@aksar/api/transformer";
+import { useAuth } from "@clerk/clerk-expo";
+//import { Text } from 'react-native'
+
+/**
+ * A set of typesafe hooks for consuming your API.
+ */
+export const trpc = createTRPCReact<AppRouter>();
 
 const getBaseUrl = () => {
   /**
@@ -18,25 +35,28 @@ const getBaseUrl = () => {
   return `http://${localhost}:3000`;
 };
 
-/**
- * A wrapper for your app that provides the TRPC context.
- * Use only in _app.tsx
- */
-import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
-import { transformer } from "@aksar/api/transformer";
-import { AppRouter, trpcNative } from "@aksar/api";
+export const TRPCProvider: React.FC<{
+  children: React.ReactNode;
+  authToken: string | null;
+}> = ({ children /*, authToken*/ }) => {
+  //authToken prop is not used here
+  //Instead, useAuth is getting token directly from clerk.
+  //Using authToken from saved state in encapsulating TRPCAuthContext causes
+  //stale token to be used, as it is saved from the context.
 
-export const TRPCProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+  const { getToken } = useAuth();
   const [queryClient] = React.useState(() => new QueryClient());
   const [trpcClient] = React.useState(() =>
-    trpcNative.createClient({
+    trpc.createClient({
       transformer,
       links: [
         httpBatchLink({
+          async headers() {
+            const token = await getToken();
+            return {
+              Authorization: token || "",
+            };
+          },
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
@@ -44,8 +64,36 @@ export const TRPCProvider: React.FC<{ children: React.ReactNode }> = ({
   );
 
   return (
-    <trpcNative.Provider client={trpcClient} queryClient={queryClient}>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpcNative.Provider>
+    </trpc.Provider>
   );
+};
+
+export const TRPCAuthContext: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  /*
+  // get JWT from clerk
+  console.log('Getting token')
+  const { getToken } = useAuth()
+  // minimal example on how to set the token in the provider
+  const [authToken, setAuthToken] = React.useState<string | null>(null)
+  useEffect(() => {
+    getToken()
+      .then((token) => {
+        setAuthToken(token)
+      })
+      .catch((err) => {
+        console.log('Error getting token', err)
+      })
+  }, [])
+  if (!authToken) return <Text>Loading</Text>
+*/
+
+  //this is not used, simply wraps the provider
+  //clerk.dev has direct access to the token, so it is not needed here
+  //using the clerk.dev getToken() is much easier and more reliable
+  //getToken automatically checks for token expiration and refreshes it
+  return <TRPCProvider authToken={null}>{children}</TRPCProvider>;
 };
